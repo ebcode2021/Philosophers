a69625f2731011a87b7bda9f8e2f99323e9b45e8
ebcode2021 <ebcode2021@gmail.com>
2023-01-23 12:40:16 +0900

    del::non-use error msg

--- philo_bonus/includes/error_bonus.h
+++ philo_bonus/includes/error_bonus.h
@@ -6,7 +6,7 @@
 /*   By: eunson <eunson@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 20:25:19 by eunson            #+#    #+#             */
-/*   Updated: 2023/01/22 16:18:51 by eunson           ###   ########.fr       */
+/*   Updated: 2023/01/23 12:40:02 by eunson           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,5 @@
 # define ARGU_CNT "Check the number of arguments"
 # define MALLOC_FAIL "Malloc Fail!"
 # define NOT_A_VALID "Invalid argument or arguments (only positive number)"
-# define THREAD_ERROR "Thread create error"
 
 #endif
